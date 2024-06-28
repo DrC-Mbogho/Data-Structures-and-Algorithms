ee211a739ad93323d22447db6d77ef1feca3c71f
DrC-Mbogho <116364889+DrC-Mbogho@users.noreply.github.com>
2024-06-28 18:46:57 +0300

    Main class for queue execution

---
+++ Queue/Queueing.java
@@ -0,0 +1,30 @@
+public class Queueing
+{
+    public static void main (String arg[])
+  {
+     MainQueue q = new MainQueue(5);
+     
+     //add items to the queue
+     q.add("001");
+     q.add("002");
+     q.add("003");
+     q.add("004");
+     q.add("005");
+     
+     //Display items in queue
+     q.print();
+     
+     //remove two items from queue
+     q.remove();
+     q.remove();
+     
+     //Display remaining items in queue
+     System.out.println("\n Remaining items");
+     q.print();
+     
+     //See front item
+     q.peek();
+  
+  }
+
+}
