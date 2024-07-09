c6533d268b59c72c415be3e10c8a61b5e2a98725
Hansel Omondi <ogopabhs@gmail.com>
2024-07-09 18:33:42 +0300

    Add: header file with declaration of queue struct
    Function signatures for the following were also declared: is_empty,
    is_full, enqueue, dequeue, display_queue and initialise_queue

---
+++ Queue/c_implementation_array/queue_array.h
@@ -0,0 +1,39 @@
+#ifndef QUEUE_ARRAY_H
+#define QUEUE_ARRAY_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * struct queue - A queue of strings
+ * @size: Maximum number of elements in the queue
+ * @front: Index of the first element in the queue
+ * @back: Index of the last element in the queue
+ * @count: Number of items currently in the queue
+ * @content: Array of strings
+ *
+ * Description: A structure of the queue abstract data type
+ */
+typedef struct queue
+{
+	int size;
+	int front;
+	int back;
+	int count;
+	char **content;
+} queue_t;
+
+
+int is_empty(queue_t *queue);
+int is_full(queue_t *queue);
+void enqueue(queue_t *queue, const char *data);
+char *dequeue(queue_t *queue);
+char *peek(queue_t *queue);
+void display_queue(queue_t *queue);
+queue_t *initialise_queue(int size);
+
+
+#endif /* ifndef QUEUE_ARRAY_H */
